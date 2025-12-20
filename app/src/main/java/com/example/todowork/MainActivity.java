aa2eb1c759ff13b2c5c88c3c15eb05f3b6ff8366
vaish_404 <vaishnavish2318@gmail.com>
2025-12-20 22:42:36 +0530

    Adding 2 layouts

--- app/src/main/java/com/example/todowork/MainActivity.java
+++ app/src/main/java/com/example/todowork/MainActivity.java
@@ -21,11 +21,6 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<String> items;
-    private ListView list;
-    private Button button;
-
-    private ArrayAdapter<String> itemsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,49 +28,9 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        list = findViewById(R.id.list);
-        button= findViewById(R.id.button);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                additem(view);
-            }
-        });
-        items=new ArrayList<>();
-        itemsAdapter= new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,items);
-        list.setAdapter(itemsAdapter);
-        /*
-        list.setOnClickListener(new AdapterView.OnItemLongClickListener()
-            @Override
-                boolean onItemLongClick(AdapterView<?> parent, View view, ){
-                    return remove(position);
-
-                 }
-            });*/
-
-    }
-/*
-    private boolean remove(int position) {
-        Context context = getApplicationContext();
-        Toast.makeText(context, "Item Removed", Toast.LENGTH_LONG).show();
-        items.remove(position);
-        itemsAdapter.notifDataSetChanged();
-        return true;
-    }*/
 
 
-    private void additem(View view){
-        EditText input= findViewById(R.id.edit_text);
-        String itemText = input.getText().toString();
 
-        if(!(itemText.equals(""))){
-            itemsAdapter.add(itemText);
-            input.setText("");
-        }
-        else{
-            Toast.makeText(getApplicationContext(), "Please Enter Text...!", Toast.LENGTH_LONG).show();
-        }
 
     }
 }
